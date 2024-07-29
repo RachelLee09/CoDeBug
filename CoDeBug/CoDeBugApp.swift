cfa56ede5ee8535ad334630f6aa44b61c56bc42d
Rachel Lee <rachellee3309@hotmail.com>
2024-07-29 15:25:12 -0700

    Initial Commit

---
+++ CoDeBug/CoDeBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoDeBugApp.swift
+//  CoDeBug
+//
+//  Created by Scholar on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct CoDeBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
