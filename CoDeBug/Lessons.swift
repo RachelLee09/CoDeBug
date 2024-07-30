b506f56e81605485be96e8bbec8adcfb77547943
Rachel Lee <rachellee3309@hotmail.com>
2024-07-30 11:14:47 -0700

    added necessary files

---
+++ CoDeBug/Lessons.swift
@@ -0,0 +1,18 @@
+//
+//  Lessons.swift
+//  CoDeBug
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct Lessons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Lessons()
+}
---
+++ CoDeBug/Resources.swift
@@ -0,0 +1,18 @@
+//
+//  Resources.swift
+//  CoDeBug
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct Resources: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Resources()
+}
