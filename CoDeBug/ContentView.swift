840274755bbd0cb7328302a1014bb42c731a000d
eden fineza <edengfineza@gmail.com>
2024-07-31 11:17:39 -0700

    eden

--- CoDeBug/ContentView.swift
+++ CoDeBug/ContentView.swift
@@ -4,15 +4,62 @@
 //
 //  Created by Scholar on 7/29/24.
 //
+//
+//  ContentView.swift
+//  CoDeBug
+//
+//  Created by Scholar on 7/29/24.
+//
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer()
+                Text("Welcome to")
+//                    .font(.custom("Andale Mono", size: 35))
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color(hue: 0.547, saturation: 1.0, brightness: 1.0))
+                Text("CoDeBug")
+//                    .font(.custom("Andale Mono", size: 65))
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(hue: 0.084, saturation: 0.331, brightness: 0.794))
+                Spacer()
+                HStack {
+                    Image("bugjump")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                        .padding(.all)
+                    Spacer()
+                }
+                
+            }
+            .background(Rectangle().frame(width: 1000, height: 1000))
+            .foregroundColor(Color(red: 0.161, green: 0.173, blue: 0.198))
+            .toolbar {
+                ToolbarItemGroup(placement: .status) {
+                    NavigationLink(destination: Lessons()) {
+                        Image("brain")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 60.0, height: 60.0)
+                    }
+                    NavigationLink(destination: Resources()) {
+                        Image("list")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 50.0, height: 50.0)
+                    }
+                }
+                
+            }
+        }
         NavigationLink(destination: Lessons().navigationBarBackButtonHidden(true)) {
             Text("Go to")
-            
-            
         }
     }
 }
